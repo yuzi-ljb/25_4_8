665867077b181f1fb1a1a3e0a90a0128bed4db7f
佳斌 李 <l3234216896@gmail.com>
2025-04-09 02:55:29 +0800

    2

--- 25_4_8/test.c
+++ 25_4_8/test.c
@@ -1,14 +1,99 @@
 #include<stdio.h>
+//
+//int main()
+//{
+//	int math[5] = {1,2,3,4,5};
+//	int i;
+//	for (i = 0; i <= 4;i++)
+//	{
+//		printf("%d",math[i]);
+//	}
+//	
+//
+//	return 0;
+//}
 
-int main()
-{
-	int math[5] = {1,2,3,4,5};
-	int i;
-	for (i = 0; i <= 4;i++)
-	{
-		printf("%d",math[i]);
-	}
-	
 
-	return 0;
-}
+//int main()
+//{
+//	int math[7];
+//	int i;
+//	for (i = 0; i <= 6; i++) {
+//		scanf_s("%d", &math[i]);
+//		
+//	}
+//	for (i = 0; i <= 6; i++) {
+//		printf("%d", math[i]);
+//
+//	}
+//	
+//	
+//	return 0;
+//}
+
+//int main()
+//
+//{
+//	int math[10] = { 0 };
+//	int sz = sizeof(math[0]);
+//	int a = sizeof(math) / sz;
+//	printf("%d", a);
+//	return 0;
+//}
+
+
+//int main()
+//
+//{
+//	int arr1[3][4] = { {1,2,3,4},{4,6,7},{8,9} };
+//	printf("%d",arr1[2][1]);
+//
+//	return 0;
+//}
+
+//int main() {
+//	int math[3][5] = { 1,2,3,4,5, 2,3,4,5,6, 3,4,5,6,7 };
+//	int a=0;
+//	
+//	for (a = 0; a < 3; a++) {
+//		int b = 0;
+//		for (b = 0; b < 5; b++) {
+//			scanf_s("%d", &math[a][b]);
+//		}
+//	}
+//	for (a = 0; a < 3; a++) {
+//		int b = 0;
+//		for (b = 0; b < 5; b++) {
+//			printf("%d", math[a][b]);
+//			
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+// 第3题
+//int main() {
+//	int math[3][3] = { {1,2,3},{4,5,5},{7,8,9} };
+//	int a;
+//	a = math[0][0] + math[1][1] + math[2][2];
+//	printf("%d", a);
+//
+//	return 0;
+//}
+
+//第4题
+//
+//int main() {
+//	int arr1[5] = { 1,2,3,4,5 };
+//	int a = 0;
+//	for (a = 0; a < 5; a++) {
+//		scanf_s("%d", &arr1[a]);
+//	}
+//	for (a = 4; a >= 0; a--) {
+//		printf("%d", arr1[a]);
+//	}
+//	return 0;
+//}
+
